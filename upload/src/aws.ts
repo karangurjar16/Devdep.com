45b74633b211c7c16cb0e9d5f848bec6a6c2be2d
Karan Gurjar <gurjarkaran03022004@gmail.com>
2025-12-16 22:51:09 +0530

    secret change

--- upload/src/aws.ts
+++ upload/src/aws.ts
@@ -1,14 +1,16 @@
 import { S3 } from "aws-sdk";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const s3 = new S3({
-    accessKeyId: "ea4bac46297743cadc6b5100c889f01f",
-    secretAccessKey: "ae5ac388e2ca22ae37927056d1d51896bb2a1ddd75badce12fd2c4de9a3f51b4",
-    endpoint: "https://5cd0f10c18c85b5b1eb72d39295a6ecf.r2.cloudflarestorage.com"
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    endpoint: process.env.R2_ENDPOINT
 })
 
-// fileName => output/12312/src/App.jsx
-// filePath => /Users/harkiratsingh/vercel/dist/output/12312/src/App.jsx
+
 export const uploadFile = async (fileName: string, localFilePath: string) => {
     const fileContent = fs.readFileSync(localFilePath);
     const response = await s3.upload({
